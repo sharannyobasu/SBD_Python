5f044ff439495de51e10b1963278a3edc8b684f8
sharannyo <yobasu2015@gmail.com>
2025-08-03 01:10:13 +0530

    02 Aug Practice Dumps

---
+++ firstProg/SparkPracticeQuestions/Combinational Questions/Question 9.py
@@ -0,0 +1,48 @@
+#Question 9: Weekend Joiner Tag, Domain Popularity, Tenure Title
+
+# Task:
+
+# Derive day_joined (Monday to Sunday)
+
+# Create weekend_joiner flag if joined on Saturday or Sunday
+
+# Check if email domain is one of popular domains: ["gmail", "yahoo", "hotmail"]
+
+# Based on tenure:
+
+# <1 year -> "Fresher"
+# 1-3 years -> "Stable"
+# 3 years -> "Long-Term"
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.functions import datediff
+
+spark=SparkSession.Builder() \
+    .master("local[*]") \
+    .appName("Question 6") \
+    .getOrCreate()
+
+
+data = [
+    ("Karthik", "karthik@gmail.com", "2023-12-12", "Data Engineer", 85000),
+    ("Veer", "veer@yahoo.com", "2022-11-01", "Business Analyst", 65000),
+    ("Veena", "veena@company.org", "2021-06-15", "Data Scientist", 105000),
+    ("Vinay", "vinay@gmail.com", "2024-01-10", "Intern", 25000),
+    ("Vijay", "vijay@hotmail.com", "2020-05-22", "Senior Manager", 125000)
+]
+
+schema="name string, email string, join_date string, designation string, salary int"
+
+df=spark.createDataFrame(data, schema)
+
+df=df.withColumn("day_joined", date_format(col("join_date"), 'EEEE'))
+df=df.withColumn("join_date", to_date(col("join_date"), "yyyy-MM-dd"))
+
+# Create weekend_joiner flag if joined on Saturday or Sunday
+df=df.withColumn("weekendJoiner", when(col(("day_joined")=='Sunday') | (col("day_joined")=='Saturday'), "No").otherwise("No"))
+
+#df=df.withColumn("tenure", when(current_date()-col(")).when().otherwise("No"))
+
+df.show()
